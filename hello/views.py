16c638ee001c6eed00c7ecdd41a5767302db7f2b
sahil <sahilsaini_22@outlook.com>
2021-01-11 18:13:55 +0100

    initial

---
+++ hello/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+
+# Create your views here.
+
+from .models import *
+from .forms import *
+
+def home(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')  
+
+    context = {
+        'tasks': tasks, 
+        'form': form
+    }
+    return render(request, 'hello/list.html', context)
+    # return HttpResponse("hello world")
+
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/') 
+
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'hello/update_task.html', context)
+
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item' : item }
+    return render(request, 'hello/delete.html', context)
+
